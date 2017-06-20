86d21e8f5c31280a6282845e11b7c568b0b2573e
Prashant Gupta <antshpra@gmail.com>
2017-06-20 16:13:46 +0000

    Adding route config

--- config/main.js
+++ config/main.js
@@ -1,28 +1,17 @@
 var config = {};
 
 config.devo = {
-  //todo for ref, delete this later
-  // 'ACCESS_TOKEN_DATASTORE_PROJECT': 'devo-pratilipi',
-  // 'PRATILIPI_IP_HOST': 'localhost',
-  // 'AUTHOR_IP_HOST': 'localhost',
-  // 'API_ORCHESTRATOR_PORT': 80,
-  // 'LOGGING_PROJECT': 'devo-pratilipi',
-  // 'METRIC_PROJECT': 'devo-pratilipi',
-  // 'SEARCH_IP_HOST': '52.221.209.219',
-  // 'SEARCH_IP_PORT': 2579,
-  // 'RECOMMENDATION_IP_HOST': '54.169.153.147',
-  // 'ILB_HOST': 'internal-pratilipi-devo-elb-internal-2113898828.ap-southeast-1.elb.amazonaws.com'
-  'LOGGING_METRIC_SERVICE_NAME': 'pag-devo-aws',
+  'LOGGING_METRIC_SERVICE_NAME': 'ecs-pag-devo',
   'AUTHENTICATION_ENDPOINT': '/auth/authorize'
 };
 
 config.gamma = {
-  'LOGGING_METRIC_SERVICE_NAME': 'pag-prod-aws',
+  'LOGGING_METRIC_SERVICE_NAME': 'ecs-pag-gamma',
   'AUTHENTICATION_ENDPOINT': '/auth/authorize'
 };
 
 config.prod = {
-  'LOGGING_METRIC_SERVICE_NAME': 'pag-prod-aws',
+  'LOGGING_METRIC_SERVICE_NAME': 'ecs-pag-prod',
   'AUTHENTICATION_ENDPOINT': '/auth/authorize'
 };
 
---
+++ config/route.js
@@ -0,0 +1,19 @@
+{
+
+  '/pratilipi/cover':{
+    'GET':{'path':'/image/pratilipi/cover','auth':false}
+  },
+
+  '/author/image':{
+    'GET':{'path':'/image/author/profile','auth':false}
+  },
+
+  '/author/cover':{
+    'GET':{'path':'/image/author/cover','auth':false}
+  },
+
+  '/search':{
+    'GET':{'path':'/search','auth':true}
+  }
+
+}
