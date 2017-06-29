7d9fc2acf3117ea9265a4ebeb6275c5c0b1f75c1
Raghu Raman <rraagghhu@gmail.com>
2017-06-29 20:12:35 +0530

    Updating route.js

--- config/route.js
+++ config/route.js
@@ -1,47 +1,47 @@
 module.exports = {
 
-  '/pratilipi/cover': {
-    'GET':{'path':'/image/pratilipi/cover', 'auth':false, 'bypassPag': true}
-  },
+	'/pratilipi/cover': {
+		'GET': { 'path': '/image/pratilipi/cover', 'auth': false }
+	},
 
-  '/author/image':{
-    'GET':{'path':'/image/author/profile', 'auth':false, 'bypassPag': true}
-  },
+	'/author/image': {
+		'GET': { 'path': '/image/author/profile', 'auth': false }
+	},
 
-  '/author/cover':{
-    'GET':{'path':'/image/author/cover', 'auth':false, 'bypassPag': true}
-  },
+	'/author/cover': {
+		'GET': { 'path': '/image/author/cover', 'auth': false }
+	},
 
-  '/image/author/cover':{
-    'GET':{'path':'/image/author/cover', 'auth':false, 'bypassPag': true}
-  },
+	'/image/author/cover': {
+		'GET': { 'path': '/image/author/cover', 'auth': false }
+	},
 
-  '/search':{
-    'GET':{'path':'/search/search', 'auth':true}
-  },
+	'/search': {
+		'GET': { 'path': '/search/search', 'auth': true }
+	},
 
-  '/search/search':{
-    'GET':{'path':'/search/search', 'auth':true}
-  },
+	'/search/search': {
+		'GET': { 'path': '/search/search', 'auth': true }
+	},
 
-  '/search/trending_search':{
-    'GET':{'path':'/search/trending_search', 'auth':true}
-  },
+	'/search/trending_search': {
+		'GET': { 'path': '/search/trending_search', 'auth': true }
+	},
 
-  '/recommendation/pratilipis':{
-    'GET':{'path':'/recommendation/pratilipis', 'auth':true}
-  },
+	'/recommendation/pratilipis': {
+		'GET': { 'path': '/recommendation/pratilipis', 'auth': true }
+	},
 
-  '/pages':{
-    'GET':{'path':'/pages', 'auth':false}
-  },
+	'/pages': {
+		'GET': { 'path': '/pages', 'auth': true }
+	},
 
-  '/pratilipis':{
-    'GET':{'path':'/pratilipis', 'auth':false}
-  },
+	'/pratilipis': {
+		'GET': { 'path': '/pratilipis', 'auth': false }
+	},
 
-  '/authors':{
-    'GET':{'path':'/authors', 'auth':false}
-  }
+	'/authors': {
+		'GET': { 'path': '/authors', 'auth': false }
+	}
 
 };
