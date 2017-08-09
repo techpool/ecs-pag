b78c18591d2a54c918d84c693fdccb964ea99819
Raghu Raman <rraagghhu@gmail.com>
2017-08-09 16:21:25 +0530

    Updating server.js

--- server.js
+++ server.js
@@ -182,7 +182,8 @@ function _getHttpPromise( uri, method, headers, body ) {
 		method: method,
 		agent : uri.indexOf( "https://" ) >= 0 ? httpsAgent : httpAgent,
 		json: true,
-		timeout: 60000, // 60 seconds
+		simple: false,
+//		timeout: 60000, // 60 seconds
 		resolveWithFullResponse: true
 	};
 	if( headers ) genericReqOptions.headers = headers;
