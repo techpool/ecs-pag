83302ba702ac1ce22ed38add121acc25e70b19c8
Raghu Raman <rraagghhu@gmail.com>
2017-09-08 13:04:37 +0530

    Updating server.js.

--- config/route.js
+++ config/route.js
@@ -236,13 +236,6 @@ module.exports = {
 		}
 	},
 
-	'/follows/v2.0/authors/followers/all': {
-		'GET': {
-			'auth': false,
-			'path': '/follows/v2.0/authors/followers/all'
-		}
-	},
-
 	'/ecs': {
 		'POST': {
 			'methods': {
