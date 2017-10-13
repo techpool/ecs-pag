3d222995f659402395f9b975013b1a2a79f822fb
Raghu Raman <rraagghhu@gmail.com>
2017-10-13 19:03:07 +0530

    Updating server.js

--- server.js
+++ server.js
@@ -286,11 +286,11 @@ function _getAuth( resource, method, primaryContentId, params, request, response
 function _getHackyAuth( resource, method, request, response ) {
 
 	// TODO : HACK to be removed once auth is fixed with social v2 apis
-	if(resource === '/reviews' || resource === '/comments' || resource === '/votes') {
-		return new Promise( function(resolve,reject) {
-			resolve(5666005993914368);
-		})
-	}
+//	if( process.env.STAGE !== 'prod' && ( resource === '/reviews' || resource === '/comments' || resource === '/votes' ) ) {
+//		return new Promise( function(resolve,reject) {
+//			resolve(5666005993914368);
+//		});
+//	}
 
 	var authParams = {};
 
