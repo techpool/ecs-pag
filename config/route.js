b70fb1b27cf70ac2941e572b7b57a5664c61705e
kaynite <sachin@pratilipi.com>
2017-10-23 13:38:42 +0530

    parameter changed to slug

--- config/route.js
+++ config/route.js
@@ -1,55 +1,55 @@
 module.exports = {
 
-	'/pratilipi/cover': {
-		'GET': {
-			'path': '/image/pratilipi/cover',
-			'auth': false,
-			'shouldPipe': true
-		},
-		'POST': {
-			'path': '/image/pratilipi/cover',
-			'shouldPipe': true,
-			'methods': {
-				'POST': {
-					'primaryKey': 'pratilipiId'
-				}
-			}
-		}
-	},
+    '/pratilipi/cover': {
+        'GET': {
+            'path': '/image/pratilipi/cover',
+            'auth': false,
+            'shouldPipe': true
+        },
+        'POST': {
+            'path': '/image/pratilipi/cover',
+            'shouldPipe': true,
+            'methods': {
+                'POST': {
+                    'primaryKey': 'pratilipiId'
+                }
+            }
+        }
+    },
 
-	'/author/image': {
-		'GET': {
-			'path': '/image/author/profile',
-			'auth': false,
-			'shouldPipe': true
-		},
-		'POST': {
-			'path': '/image/author/profile',
-			'shouldPipe': true,
-			'methods': {
-				'POST': {
-					'primaryKey': 'authorId'
-				}
-			}
-		}
-	},
+    '/author/image': {
+        'GET': {
+            'path': '/image/author/profile',
+            'auth': false,
+            'shouldPipe': true
+        },
+        'POST': {
+            'path': '/image/author/profile',
+            'shouldPipe': true,
+            'methods': {
+                'POST': {
+                    'primaryKey': 'authorId'
+                }
+            }
+        }
+    },
 
-	'/author/cover': {
-		'GET': {
-			'path': '/image/author/cover',
-			'auth': false,
-			'shouldPipe': true
-		},
-		'POST': {
-			'path': '/image/author/cover',
-			'shouldPipe': true,
-			'methods': {
-				'POST': {
-					'primaryKey': 'authorId'
-				}
-			}
-		}
-	},
+    '/author/cover': {
+        'GET': {
+            'path': '/image/author/cover',
+            'auth': false,
+            'shouldPipe': true
+        },
+        'POST': {
+            'path': '/image/author/cover',
+            'shouldPipe': true,
+            'methods': {
+                'POST': {
+                    'primaryKey': 'authorId'
+                }
+            }
+        }
+    },
 
 	'/author/image/remove': {
 		'POST': {
@@ -79,123 +79,133 @@ module.exports = {
 		}
 	},
 
-	'/search': {
-		'GET': { 'path': '/search/search', 'auth': true }
-	},
+    '/search': {
+        'GET': {'path': '/search/search', 'auth': true}
+    },
 
-	'/search/search': {
-		'GET': { 'path': '/search/search', 'auth': true }
-	},
+    '/search/search': {
+        'GET': {'path': '/search/search', 'auth': true}
+    },
 
-	'/search/trending_search': {
-		'GET': { 'path': '/search/trending_search', 'auth': false }
-	},
+    '/search/trending_search': {
+        'GET': {'path': '/search/trending_search', 'auth': false}
+    },
 
-	'/recommendation/pratilipis': {
-		'GET': { 'path': '/recommendation/pratilipis', 'auth': false }
-	},
+    '/recommendation/pratilipis': {
+        'GET': {'path': '/recommendation/pratilipis', 'auth': false}
+    },
 
-	'/page': {
-		'GET': { 'path': '/pages', 'auth': false }
-	},
+    '/page': {
+        'GET': {'path': '/pages', 'auth': false}
+    },
 
-	'/pratilipi': {
-		'GET': {
-			'path': '/pratilipis/$primaryContentId',
-			'auth': true,
-			'primaryKey': 'pratilipiId'
-		},
-		'POST': {
-			'methods': {
-				'DELETE': {
-					'path': '/pratilipis/$primaryContentId',
-					'primaryKey': 'pratilipiId',
-					'requiredFields': [
-						{ 'pratilipiId': null },
-						{ 'state': 'DELETED' }
-					]
-				},
-				'PATCH': {
-					'path': '/pratilipis/$primaryContentId',
-					'primaryKey': 'pratilipiId',
-					'requiredFields': [
-						{ 'pratilipiId': null }
-					]
-				},
-				'POST': {
-					'path': '/pratilipis',
-					'requiredFields': [
-						{ 'language': null },
-						{ 'type': null }
-					]
-				}
-			}
-		}
-	},
+    '/pratilipi': {
+        'GET': {
+            'path': '/pratilipis/$primaryContentId',
+            'auth': true,
+            'primaryKey': 'pratilipiId'
+        },
+        'POST': {
+            'methods': {
+                'DELETE': {
+                    'path': '/pratilipis/$primaryContentId',
+                    'primaryKey': 'pratilipiId',
+                    'requiredFields': [
+                        {'pratilipiId': null},
+                        {'state': 'DELETED'}
+                    ]
+                },
+                'PATCH': {
+                    'path': '/pratilipis/$primaryContentId',
+                    'primaryKey': 'pratilipiId',
+                    'requiredFields': [
+                        {'pratilipiId': null}
+                    ]
+                },
+                'POST': {
+                    'path': '/pratilipis',
+                    'requiredFields': [
+                        {'language': null},
+                        {'type': null}
+                    ]
+                }
+            }
+        }
+    },
 
-	'/author': {
-		'GET': {
-			'path': '/authors/$primaryContentId',
-			'auth': true,
-			'primaryKey': 'authorId'
-		},
-		'POST': {
-			'methods': {
-				'PATCH': {
-					'path': '/authors/$primaryContentId',
-					'primaryKey': 'authorId',
-					'requiredFields': [
-						{ 'authorId': null }
-					],
-				},
-				'POST': {
-					'path': '/authors',
-					'requiredFields': [
-						{ 'name': null },
-						{ 'language': null },
-						{ 'userId': 0 } // Only AEEs can add Authors
-					]
-				}
-			}
-		}
-	},
+    '/author': {
+        'GET': {
+            'path': '/authors/$primaryContentId',
+            'auth': true,
+            'primaryKey': 'authorId'
+        },
+        'POST': {
+            'methods': {
+                'PATCH': {
+                    'path': '/authors/$primaryContentId',
+                    'primaryKey': 'authorId',
+                    'requiredFields': [
+                        {'authorId': null}
+                    ],
+                },
+                'POST': {
+                    'path': '/authors',
+                    'requiredFields': [
+                        {'name': null},
+                        {'language': null},
+                        {'userId': 0} // Only AEEs can add Authors
+                    ]
+                }
+            }
+        }
+    },
+
+    '/authorNew': {
+        'GET': {
+            'path': '/authors',
+            'auth': false,
+            'requiredFields': [
+                {'slug': null}
+            ]
+        }
+    },
 
 	'/user-activity/is_add_to_lib': {
 		'GET': { 'path': '/user-activity/is_add_to_lib', 'auth': false }
 	},
 
-	'/user-activity/is_following_author': {
-		'GET': { 'path': '/user-activity/is_following_author', 'auth': false }
-	},
+    '/user-activity/is_following_author': {
+        'GET': {'path': '/user-activity/is_following_author', 'auth': false}
+    },
 
-	'/pratilipi/tags/update': {
-		'POST': {
-			'methods': {
-				'PATCH': {
-					'path': '/pratilipis/$primaryContentId',
-					'primaryKey': 'pratilipiId',
-					'requiredFields': [
-						{ 'pratilipiId': null }
-					]
-				}
-			}
-		}
-	},
+    '/pratilipi/tags/update': {
+        'POST': {
+            'methods': {
+                'PATCH': {
+                    'path': '/pratilipis/$primaryContentId',
+                    'primaryKey': 'pratilipiId',
+                    'requiredFields': [
+                        {'pratilipiId': null}
+                    ]
+                }
+            }
+        }
+    },
 
-	'/pratilipi/list': {
-		'GET': {
-			'path': '/pratilipis',
-			'auth': true,
-			'requiredFields': [
-				{ 'authorId': null },
-				{ 'state': null },
-				{ '_apiVer': "3" }
-			],
-			'copyParam': [
-				{ 'resultCount': 'limit' }
-			]
-		}
-	},
+    '/pratilipi/list': {
+        'GET': {
+            'path': '/pratilipis',
+            'auth': true,
+            'requiredFields': [
+                {'authorId': null},
+                {'state': null},
+                {'_apiVer': "3"}
+            ],
+            'copyParam': [
+                {'resultCount': 'limit'}
+            ]
+        }
+    },
 
 	'/userauthor/follow': {
 		'GET': {
